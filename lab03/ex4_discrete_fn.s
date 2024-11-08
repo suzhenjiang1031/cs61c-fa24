cfb2d9fff7656a41beeab6a74807e40f1780aedc
suzhenjiang <suzhenjiang1031@gmail.com>
2024-11-08 19:35:28 +0800

    ex4_discrete_fn

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -6,6 +6,10 @@
 # The return value should be stored in a0
 f:
     # Your code here
+    addi t0 a0 3
+    slli t1 t0 2
+    add t1 t1 a1
+    lw a0 0(t1)
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
